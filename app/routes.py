dce5af9a2f6c103792e91270c5c5a0fac080be94
Lindsay <lindsaybolzwilson@gmail.com>
2023-04-21 10:32:53 -0400

    Created crystals class and blueprint

---
+++ app/routes.py
@@ -0,0 +1,18 @@
+from flask import Blueprint, jsonify
+
+class Crystal:
+    def __init__(self, id, name, color, powers):
+        self.id = id
+        self.name = name
+        self.color = color
+        self.powers = powers
+
+# create a list of crystals
+crystals = [
+    Crystal(1, 'Amethyst', 'Purple', 'Infinite knowledge and wisdom'),
+    Crystal(2, "Tiger's Eye", 'Gold', 'Confidence, strength'),
+    Crystal(3, "Rose Quarts", "Pink", "Love")
+]
+
+crystal_bp = Blueprint("crystals", __name__, url_prefix="/crystals")
+
