effa99e66547dd7e2a3e9be468ca20303a3d23e4
Lindsay <lindsaybolzwilson@gmail.com>
2023-04-21 10:33:13 -0400

    registered rystals blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -4,4 +4,8 @@ def create_app():
     # __name__ stores the name of the module we're in
     app = Flask(__name__)
 
+    # register routes
+    from .routes import crystal_bp
+    app.register_blueprint(crystal_bp)
+
     return app
